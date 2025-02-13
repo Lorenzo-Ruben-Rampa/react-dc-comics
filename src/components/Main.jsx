edc49a6b0a003f3f4b018240e4b772ba501d6c0a
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-13 07:45:03 +0100

    non funziona nulla

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <main>
+            <section id="products">
+                <h2>Lista prodotti</h2>
+            </section>
+        </main>
+    );
+}
