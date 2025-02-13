d90f0452f19027a1009c71a78c36bf837e02630f
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-13 16:47:41 +0100

    bonus completato

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,18 @@
 export default function Banner() {
     return (
         <div className="banner">
-            <div className="container">
+            <div className="container banner-container">
                 <div className="banner-container">
-                    <img src="/img/buy-comics-digital-comics.png" alt="DC-comics-book-logo">
+                    <img className="banner-img" src="/img/buy-comics-digital-comics.png" alt="DC-comics-book-logo"></img>
+                    <p>DIGITAL COMICS</p>
+                    <img className="banner-img" src="/img/buy-comics-merchandise.png" alt="DC-comics-shirt-logo"></img>
+                    <p>DC MERCHANDISE</p>
+                    <img className="banner-img" src="/img/buy-comics-subscriptions.png" alt="DC-subscriptions-logo"></img>
+                    <p>SUBSCRIPTION</p>
+                    <img className="banner-img" src="/img/buy-comics-shop-locator.png" alt="geoloc-logo"></img>
+                    <p>COMIC SHOP LOCATOR</p>
+                    <img className="banner-img" src="/img/buy-dc-power-visa.png" alt="visa-card-logo"></img>
+                    <p>DC POWER VISA</p>
                 </div>
             </div>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,14 @@
-import Banner from "/Banner.jsx";
+import Banner from "./Banner.jsx";
 
 export default function Main() {
     return (
         <main>
-            <section id="products">
-                <div className="container">
+            <div className="container">
+                <div className="add-content">
                     <h2>--&gt; Content goes here &lt;--</h2>
                 </div>
-            </section>
+            </div>
+            <Banner />
         </main>
     );
 }
