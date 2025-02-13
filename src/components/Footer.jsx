d0563ea6dcbd0d50e584f3ad2c3e7800437687c5
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-13 13:14:24 +0100

    impaginazione iniziale generale

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,68 @@
 export default function Footer() {
     return (
-        <footer className="container">
-            <nav className="footer-nav">
-                <div className="footer-col">
-                    <figure>
-                        <img src="/logo.png" alt="La Molisana" />
-                    </figure>
-                </div>
-                <div className="footer-col">
-                    <div>
-                        <h4>Pastificio</h4>
-                        <ul>
-                            <li><a href="#">Il Pastificio</a></li>
-                            <li><a href="#">Grano</a></li>
-                            <li><a href="#">Filiera</a></li>
-                            <li><a href="#">100 anni di pasta</a></li>
-                            <li><a href="#">Sartoria della pasta</a></li>
-                        </ul>
+        <footer>
+            <div className="container">
+                <nav className="footer-nav">
+                    <div className="footer-col">
+                        <figure>
+                            <img src="/img/dc-logo-bg.png" alt="Dc-Logo" />
+                        </figure>
                     </div>
-                </div>
-                <div className="footer-col">
-                    <div>
-                        <h4>Prodotti</h4>
-                        <ul>
-                            <li><a href="#">Le classiche</a></li>
-                            <li><a href="#">Le Integrali</a></li>
-                            <li><a href="#">Le Speciali</a></li>
-                            <li><a href="#">Le Biologiche</a></li>
-                            <li><a href="#">Le Gluten-free</a></li>
-                        </ul>
+                    <div className="footer-col">
+                        <div>
+                            <h4>DC COMICS</h4>
+                            <ul>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                    <div className="footer-col">
+                        <div>
+                            <h4>DC</h4>
+                            <ul>
+                                <li><a href="#">Terms of Use</a></li>
+                                <li><a href="#">Privacy policy(New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="footer-col">
+                        <div>
+                            <h4>SITES</h4>
+                            <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="footer-col">
+                        <div>
+                            <h4>SHOP</h4>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </footer>
     );
 }
