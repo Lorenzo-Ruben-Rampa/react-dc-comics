55c63397d91b699b2d3c3609e35ebac7ec0b353f
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-14 19:52:59 +0100

    correzioni css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import Banner from "./Banner.jsx";
 export default function Main() {
     return (
         <main>
+            <div class="teen-titans"></div>
             <div class="array-container">
                 <div className="container">
                     <button>LOAD MORE</button>
